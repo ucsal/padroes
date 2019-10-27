8506d9ac8074e0396c25c38f12c895fab32eee68
Mario Jorge Pereira <mariojp@gmail.com>
2019-10-27 11:01:35 -0300

    Repatorando para Maven

---
+++ editorfigura/src/main/java/ws/domore/editorfigura/model/Figura.java
@@ -0,0 +1,17 @@
+package ws.domore.editorfigura.model;
+
+import java.awt.*;
+
+public class Figura {
+	
+	private int altura;
+	private int largura;
+	
+	private int x;
+	private int y;
+	
+	private Color corLinha;
+	private Color corPreenchiento;
+
+
+}
