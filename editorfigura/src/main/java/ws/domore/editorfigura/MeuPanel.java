ad1174085d83753b9b7042fa570d6cddbfa1ed49
Mario Jorge Pereira <mariojp@gmail.com>
2019-10-27 11:47:39 -0300

    REFATORANDO

--- editorfigura/src/main/java/ws/domore/editorfigura/MeuPanel.java
+++ editorfigura/src/main/java/ws/domore/editorfigura/MeuPanel.java
@@ -1,16 +1,20 @@
 package ws.domore.editorfigura;
 
-import javax.swing.*;
-import java.awt.*;
-import java.util.Set;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.LayoutManager;
+import java.awt.Shape;
+import java.util.Collection;
+
+import javax.swing.JPanel;
 
 class MeuPanel extends JPanel {
 
-    private Set<Shape> figuras;
+    private Collection<Shape> figuras;
 
     private static final long serialVersionUID = 1L;
 
-    public MeuPanel(LayoutManager layoutManager, Set<Shape> figuras) {
+    public MeuPanel(LayoutManager layoutManager, Collection<Shape> figuras) {
         super(layoutManager,true);
         this.figuras = figuras;
     }
--- editorfigura/src/main/java/ws/domore/editorfigura/Principal.java
+++ editorfigura/src/main/java/ws/domore/editorfigura/Principal.java
@@ -1,12 +1,8 @@
 package ws.domore.editorfigura;
 
-
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.LayoutManager;
 import java.awt.Shape;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -14,87 +10,82 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.geom.Ellipse2D;
-import java.awt.geom.Rectangle2D;
-import java.util.HashSet;
-import java.util.Set;
 import java.awt.geom.Line2D;
+import java.awt.geom.Rectangle2D;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-
-import javax.swing.DebugGraphics;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
 
 /**
  *
  * @author mariojp
  */
-public class Principal extends JFrame implements ActionListener,MouseListener {
+public class Principal extends JFrame implements ActionListener, MouseListener {
 
-    
-    /**
+	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private JButton botaoQuadrado = new JButton("QUADRADO");
 	private JButton botaoCirculo = new JButton("CIRCULO");
-	private JButton botaoApagar = new JButton("APAGAR");
 	private JButton botaoTriangulo = new JButton("TRIANGULO");
-  private JButton botaoLimparUltimoDesenho = new JButton("LIMPAR ULTIMO OBJETO");
+	private JButton botaoLimparUltimoDesenho = new JButton("VOLTAR");
+	private JButton botaoApagar = new JButton("APAGAR");
+
 
 	private String selecionado = "";
 
-	private Set<Shape> figuras = new HashSet<Shape>();
-	
-	//Criando um JPanel com layoutManager null
-    private JPanel painel = new MeuPanel(null, figuras);
+	private List<Shape> figuras = new ArrayList<Shape>();
 
-    
-    public Principal() {
-        //ITTULO DA JANELA
-        setTitle("FIGURAS");
-        
-        //Interface
-        JPanel botoes = new JPanel();
-        botoes.setLayout(new GridLayout(5, 1));
-        botoes.add(botaoQuadrado);
-        botoes.add(botaoCirculo);
-      	botoes.add(botaoTriangulo);
-    		botoes.add(botaoLimparUltimoDesenho);
-        botoes.add(botaoApagar);
+	// Criando um JPanel com layoutManager null
+	private JPanel painel = new MeuPanel(null, figuras);
 
-        JPanel lateral = new JPanel();
-        lateral.add(botoes);      		
-      		
-        //Painel lateral
-        this.add(BorderLayout.WEST, lateral);
-        //centro
-        
-        painel.setBackground(Color.WHITE);
-        this.add(BorderLayout.CENTER, painel);
+	public Principal() {
+		// ITTULO DA JANELA
+		setTitle("FIGURAS");
 
-        
-        //ACOES
-        botaoCirculo.addActionListener(this);
-        botaoQuadrado.addActionListener(this);
-        botaoTriangulo.addActionListener(this);
-        botaoApagar.addActionListener(this);
-        botaoLimparUltimoDesenho.addActionListener(this);
+		// Interface
+		JPanel botoes = new JPanel();
+		botoes.setLayout(new GridLayout(5, 1));
+		botoes.add(botaoQuadrado);
+		botoes.add(botaoCirculo);
+		botoes.add(botaoTriangulo);
+		botoes.add(botaoLimparUltimoDesenho);
+		botoes.add(botaoApagar);
 
+		JPanel lateral = new JPanel();
+		lateral.add(botoes);
 
-        painel.addMouseListener(this);
+		// Painel lateral
+		this.add(BorderLayout.WEST, lateral);
+		// centro
 
-        this.setSize(Toolkit.getDefaultToolkit().getScreenSize().width/2, Toolkit.getDefaultToolkit().getScreenSize().height/2);
-        
-        setLocationRelativeTo(null);
+		painel.setBackground(Color.WHITE);
+		this.add(BorderLayout.CENTER, painel);
 
-        this.setVisible(true);
-        this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-    }
+		// ACOES
+		botaoCirculo.addActionListener(this);
+		botaoQuadrado.addActionListener(this);
+		botaoTriangulo.addActionListener(this);
+		botaoApagar.addActionListener(this);
+		botaoLimparUltimoDesenho.addActionListener(this);
+
+		painel.addMouseListener(this);
+
+		this.setSize(Toolkit.getDefaultToolkit().getScreenSize().width / 2,
+				Toolkit.getDefaultToolkit().getScreenSize().height / 2);
+
+		setLocationRelativeTo(null);
+
+		this.setVisible(true);
+		this.setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+	}
 
 	public void actionPerformed(ActionEvent e) {
 		int ultimo = 0;
@@ -106,72 +97,65 @@ public class Principal extends JFrame implements ActionListener,MouseListener {
 		if (botao.getText().contains("CIRCULO")) {
 			selecionado = "CIRCULO";
 		}
-    if(botao.getText().contains("TRIANGULO")) {
+		if (botao.getText().contains("TRIANGULO")) {
 			selecionado = "TRIANGULO";
 		}
-     if(botao.getText().contains("APAGAR")) {
-        figuras.clear();
-    		selecionado = "";
-     }
-		if (botao.getText().contains("LIMPAR ULTIMO OBJETO")) {
-  		selecionado = "";
-
-			if (!figuras.isEmpty()) {
-				ultimo = 0;
-				for (Shape shape : figuras) {
-					ultimo++;
-				}
-				for (Shape shape : figuras) {
-					i++;
-					if (ultimo == i) {
-						figuras.remove(shape);
-					}
-				}
-			}
-			this.painel.updateUI();
+		if (botao.getText().contains("APAGAR")) {
+			figuras.clear();
+			selecionado = "";
 		}
+		if (botao.getText().contains("VOLTAR")) {
+			//FIX NOT WORK FOR TRIANGULO
+			selecionado = "";
+			if (!figuras.isEmpty()) {
+				figuras.remove(figuras.size()-1);
+			}
+		}
+		
+		this.painel.updateUI();
+
 	}
 
-    public static void main(String[] args) {
-        Principal j = new Principal();
-        j.setVisible(true);
+	public static void main(String[] args) {
+		Principal j = new Principal();
+		j.setVisible(true);
 
-    }
+	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-        int x = e.getX();
-        int y = e.getY();
-        if(selecionado.contentEquals("CIRCULO")) {
-        	figuras.add(new Ellipse2D.Double(x, y, 10, 10));
-        }else if(selecionado.contentEquals("QUADRADO")) {
-        	figuras.add(new Rectangle2D.Double(x, y, 10, 10));
-        }else if(selecionado.contentEquals("TRIANGULO")) {
+		int x = e.getX();
+		int y = e.getY();
+		if (selecionado.contentEquals("CIRCULO")) {
+			figuras.add(new Ellipse2D.Double(x, y, 10, 10));
+		} else if (selecionado.contentEquals("QUADRADO")) {
+			figuras.add(new Rectangle2D.Double(x, y, 10, 10));
+		} else if (selecionado.contentEquals("TRIANGULO")) {
 			figuras.add(new Line2D.Double(x - 10, y, x, y));
 			figuras.add(new Line2D.Double(x - 10, y - 10, x, y));
 			figuras.add(new Line2D.Double(x - 10, y - 10, x - 10, y));
 		}
-        this.painel.updateUI();		
+		this.painel.updateUI();
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		
+
 	}
-	
+
 }
