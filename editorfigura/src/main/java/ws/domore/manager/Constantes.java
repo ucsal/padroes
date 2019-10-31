06cef89ce722fdd064dde10de2d56374af82813b
Ícaro Santana Silva <kiensantana@gmail.com>
2019-10-31 08:28:42 -0300

    refatoração path solução de contorno caminho constantes.properties

--- editorfigura/src/main/java/ws/domore/manager/Constantes.java
+++ editorfigura/src/main/java/ws/domore/manager/Constantes.java
@@ -1,20 +1,16 @@
 package ws.domore.manager;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Constantes {
 	
-//	src/main/java/ws/domore/manager/Constantes.java
-//	src/main/resources/constantes.properties
-	
-	public static String getProp(String key)  {
+	public String getProp(String key)  {
 		try {
 			Properties properties = new Properties();
-			FileInputStream in = new FileInputStream("src/main/resources/constantes.properties");
-			properties.load(in);
-			in.close();
+			properties.load(this.getClass().getClassLoader().getResourceAsStream("constantes.properties"));
+			
+			//in.close();
 			return properties.getProperty(key);
 		} catch (IOException e) {
 			e.printStackTrace();
